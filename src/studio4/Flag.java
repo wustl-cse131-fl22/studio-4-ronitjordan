0ea6cd519805d870d95ffbff50352e5796287fa9
jordanprohens <jordanprohens@localhost>
2022-10-12 15:46:56 -0500

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,14 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setXscale(0,22);
+		StdDraw.setYscale(0,16);
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.rectangle(11, 8, 8, 4);
+		StdDraw.setPenColor(200,0,0);
+		StdDraw.filledEllipse(6,8.25,1.75,1);
+		StdDraw.setPenColor(0,0,200);
+		StdDraw.filledEllipse(0, 0, 0, 0);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -13,12 +13,32 @@ import edu.princeton.cs.introcs.StdDraw;
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class InterpretDrawingFile {
-
+	
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int colorOne = in.nextInt();
+		int colorTwo = in.nextInt();
+		int colorThree = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double width = in.nextDouble();
+		double length = in.nextDouble();
+		StdDraw.setPenColor(colorOne,colorTwo,colorThree);
 		
+		if(shape.equals("rectangle")) {
+			if (filled==true)
+			{
+			StdDraw.filledRectangle(x, y, width, length);
+			}else {
+				StdDraw.rectangle(x,y,width,length);
+			}
+		}
 	}
+
 }
